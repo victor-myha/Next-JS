30a7681bb0979b13b7a136d1f0d36d942fd57ebc
victor-myha <victormyhaletsky@gmail.com>
2023-02-16 01:06:30 +0200

    add getStaticProps and request

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,37 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+
+import Layout from '../componets/layout';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
-    return (
-        <Layout home>
-            <Head>
-                <title>{siteTitle}</title>
-            </Head>
-            <section className={utilStyles.headingMd}>
-                <p>Front-end</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
-            </section>
-        </Layout>
-    );
+export async function getStaticProps() {
+  const res = await fetch('https://dummyjson.com/products');
+  const { products } = await res.json();
+
+  return {
+    props: { products },
+  };
+}
+
+export default function Home({ products }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>Products Home</title>
+      </Head>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Products</h2>
+        <ul className={utilStyles.list}>
+          {products.map(({ id, title, description }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {description}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,18 @@
-import Link from 'next/link';
-import Head from "next/head";
-import Layout from "../../componets/layout";
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Layout from '../../componets/layout'
 
 export default function FirstPost() {
-    return (
-        <Layout>
-            <Head>
-                <title>
-                    Hello
-                </title>
-            </Head>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">Back to home</Link>
-            </h2>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Head>
+        <title>Hello</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href='/'>Back to home</Link>
+      </h2>
+    </Layout>
+  )
 }
